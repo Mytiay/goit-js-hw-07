99d67a565115ad4136d4b10625977a11fd31eef7
Дмитрий <dimas.savin1990@gmail.com>
2022-11-08 21:17:36 -0500

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,12 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryCards = document.querySelector(".gallery");
+galleryCards.addEventListener("click", onClickShowModal);
 
-console.log(galleryItems);
+const galleryMarkup = galleryItems.map(galleryCardMarkup).join("");
 
-// Выполняй это задание в файлах 01-gallery.html и 01-gallery.js. Разбей его на несколько подзадач:
+galleryCards.insertAdjacentHTML("afterbegin", galleryMarkup);
 
-// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
-// Реализация делегирования на div.gallery и получение url большого изображения.
-// Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
-// Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
-// Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
+function galleryCardMarkup({ preview, original, description }) {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+      </a> 
+  </div>`;
+}
